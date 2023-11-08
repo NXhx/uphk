25d9230a240a6020418fa77244052b88d62aeddd
NXhx <101697539+NXhx@users.noreply.github.com>
2023-11-08 15:31:00 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -456,7 +456,7 @@ if ospath.exists("accounts.zip"):
         srun(["rm", "-rf", "accounts"])
     srun(["7z", "x", "-o.", "-aoa", "accounts.zip", "accounts/*.json"])
     srun(["chmod", "-R", "777", "accounts"])
-    osremove('accounts.zip')
+    osremove("accounts.zip")
 if not ospath.exists('accounts'):
     config_dict['USE_SERVICE_ACCOUNTS'] = False
 sleep(0.5)
