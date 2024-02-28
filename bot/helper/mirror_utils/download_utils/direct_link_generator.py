baf42a645d849be027bcfb72fa76ec94cbc90581
NXhx <101697539+NXhx@users.noreply.github.com>
2024-02-28 11:02:27 +0700

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -91,6 +91,7 @@ def direct_link_generator(link):
             "do0od.com",
             "d0000d.com",
             "d000d.com",
+            "ds2video.com",
         ]
     ):
         return doods(link)
